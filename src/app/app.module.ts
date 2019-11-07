665199c75a09aa8d3745465301a708696f040da2
prettygal <esthernamaganda01@gmail.com>
2019-11-07 22:37:44 +0300

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import {ProductService} from './product.service';
 import {ProductTracklistingComponent} from './product-tracklisting/product-tracklisting.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import {ProductTracklistingComponent} from './product-tracklisting/product-track
     ProductPageComponent,
     ProductDescriptionComponent,
     ProductTracklistingComponent,
+    ProductListComponent,
   ],
   imports: [
     BrowserModule,
